5d4aa2a5b3f3866c4ec7fef7a5b3b67b0c2a7f06
Christian Oliveira <christian.oliveira@aged.ma.gov.br>
2019-11-01 17:59:51 -0300

    criando pagina de contato e form de contato

--- index.php
+++ index.php
@@ -45,117 +45,18 @@
         </div>
     </header>
 
-    <section class="banner-principal">
-        <div class="overlay"></div>
-        <div class="center">
-            <form>
-                <h2>Qual o seu melhor E-mail?</h2>
-                <input type="email" name="email" placeholder="Digite aqui seu melhor E-mail" required/>
-                <input type="submit" name="acao" value="Cadastrar"/>
-            </form>
-        </div>
-    </section>
+    <?php
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
-    <section class="descricao-autor">
-        <div class="center">
-            <div class="w50 left">
-                <h2>Christian Oliveira</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. 
-                O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, 
-                quando uma misturou os caracteres de um texto para criar um espécime de livro. 
-                Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. 
-                Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, 
-                e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
-            </div>
-            <div class="w50 left">
-                <img class="right" src="<?php echo INCLUDE_PATH; ?>images/user.jpg"/>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </section>
+        if (file_exists('pages/'.$url.'.php')){
+            include('pages/'.$url.'.php');
+        }else{
+            $pagina404 = true;
+            include('pages/erros/404.php');
+        }
+    ?>
 
-    <section class="especialidades">
-        <div class="center">
-            <h2 class="title">Especialidades</h2>
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-css3-alt"></i></h3>
-                <h4>CSS</h4>
-                <p>Lorem ipsum is placeholder text commonly used in the graphic, 
-                print, and publishing industries for previewing 
-                layouts and visual mockups.</p>
-            </div>
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-html5"></i></h3>
-                <h4>HTML</h4>
-                <p>Lorem ipsum is placeholder text commonly used in the graphic, 
-                print, and publishing industries for previewing 
-                layouts and visual mockups.</p>
-            </div>
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-js-square"></i></h3>
-                <h4>Javascript</h4>
-                <p>Lorem ipsum is placeholder text commonly used in the graphic, 
-                print, and publishing industries for previewing
-                layouts and visual mockups.</p>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </section>
-
-    <section class="extras">
-        <div class="center">
-            <div class="depoimentos-container w50 left">
-                <h2 class="title">Depoimentos</h2>
-                <div class="depoimento-single">
-                    <p class="descricao">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore 
-                    et dolore magna aliqua.”</p>
-                    <p class="autor">Autor: Fulado da Silva</p>
-                </div>
-                <div class="depoimento-single">
-                    <p class="descricao">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore 
-                    et dolore magna aliqua.”</p>
-                    <p class="autor">Autor: Ciclano da Ferreira</p>
-                </div>
-                <div class="depoimento-single">
-                    <p class="descricao">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore 
-                    et dolore magna aliqua.”</p>
-                    <p class="autor">Autor: Fulano da Costa</p>
-                </div>
-                <div class="depoimento-single">
-                    <p class="descricao">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore 
-                    et dolore magna aliqua.”</p>
-                    <p class="autor">Autor: Beltrano de Souza</p>
-                </div>
-            </div>
-            <div class="servicos-container w50 left">
-                <h2 class="title">Serviços<h2>
-                <ul>
-                    <li>Lorem ipsum is placeholder text commonly used in the graphic, print, 
-                    and publishing industries for previewing layouts and visual mockups.</li>
-                    <li>Lorem ipsum is placeholder text commonly used in the graphic, print, 
-                    and publishing industries for previewing layouts and visual mockups.</li>
-                    <li>Lorem ipsum is placeholder text commonly used in the graphic, print, 
-                    and publishing industries for previewing layouts and visual mockups.</li>
-                    <li>Lorem ipsum is placeholder text commonly used in the graphic, print, 
-                    and publishing industries for previewing layouts and visual mockups.</li>
-                    <li>Lorem ipsum is placeholder text commonly used in the graphic, print, 
-                    and publishing industries for previewing layouts and visual mockups.</li>
-                </ul>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </section>
-
-    <footer>
+    <footer <?php if(isset($pagina404) && $pagina404 == true) echo 'class="fixed"'; ?>>
         <div class="center">
             <p>Todos os direitos reservados, 2019.</p>
         </div>
---
+++ pages/erros/404.php
@@ -0,0 +1,6 @@
+<section class="erro404">
+    <div class="center">
+        <h2><i class="fas fa-exclamation-triangle"></i> A Página Não Existe!</h2>
+        <p>Deseja voltar para a <a href="<?php echo INCLUDE_PATH; ?>">página inicial</a>?</p>
+    </div>
+</section>
